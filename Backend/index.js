3774d99d70b61a43949d4a2e908c9665ed6226be
Estevan Usuay <estevan.usuay962@ist17dejulio.edu.ec>
2025-04-22 08:58:15 -0500

    PRACTICA VITE
    PRACTICA VITE - HERRAMIENTAS

---
+++ Backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3030;
+
+// Habilita CORS para todas las rutas y orígenes
+app.use(cors());
+
+// Ruta de prueba
+app.get('/ping', (req, res) => {
+  res.json({ message: 'Pong desde el servidor Express' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});

18096615f7e1ed62fcccbdb97ed68531709e2d5d
Estevan Usuay <estevan.usuay962@ist17dejulio.edu.ec>
2025-05-20 09:15:25 -0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,12 +6,28 @@ const PORT = 3030;
 
 // Habilita CORS para todas las rutas y orígenes
 app.use(cors());
+app.use(express.json()); // Permite recibir JSON en POST
 
 // Ruta de prueba
 app.get('/ping', (req, res) => {
   res.json({ message: 'Pong desde el servidor - USUAY ESTEVAN' });
 });
 
+// Ruta de prueba (POST)
+app.post('/ping', (req, res) => {
+  res.json({ message: 'Pong desde el servidor - USUAY ESTEVAN' });
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
+
+// Ruta principal
+app.get('/', (req, res) => {
+    res.send('¡Hola Mundo con Node.js y Express!');
+});
+
+app.post('/', (req, res) => {
+    res.send('¡Hola Mundo con Node.js y Express (POST)!');
+});
+

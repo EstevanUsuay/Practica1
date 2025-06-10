914795e386bf1eb47a9677ed4da55dd441849c0d
Estevan Usuay <estevan.usuay962@ist17dejulio.edu.ec>
2025-06-10 09:44:52 -0500

    PRACTICA HONO

--- BackendMariaDB/src/index.ts
+++ BackendMariaDB/src/index.ts
@@ -1,15 +1,27 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
-const app = new Hono()
+// importar routers desde los módulos
+import ping from '../src/ping/ping.js'
+import greet from '../src/greet/greet.js'
 
-app.get('/', (c) => {
+// usar "server" en lugar de "app" por consistencia
+const server = new Hono()
+
+server.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+// montar los routers
+server.route('/', ping)
+server.route('/', greet)
+
+const port = 3000
+console.log(`Server is running on port ${port}`)
+
 serve({
-  fetch: app.fetch,
-  port: 3000
+  fetch: server.fetch,
+  port
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
